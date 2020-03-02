74dff459445b523855f0122290670c64a1084eec
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-03-02 13:27:34 +0700

    initial commit
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

---
+++ FilePlayback/FilePlayback/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FilePlayback.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FILEPLAYBACK_DIALOG         102
+#define IDR_MAINFRAME                   128
+#define IDR_ACCELERATOR1                136
+#define IDC_OUTPUT_DEVICE_COMBO         1007
+#define IDC_PREVIEW_BOX                 1008
+#define IDC_POSITION_SLIDER             1009
+#define IDC_OPEN_BUTTON                 1010
+#define IDC_PLAY_BUTTON                 1011
+#define IDC_FILETITLE_EDIT              1012
+#define IDC_DURATION_EDIT               1013
+#define IDC_POSITION_EDIT               1014
+#define IDC_VIDEO_FORMAT_EDIT           1015
+#define IDC_LOOP                        1017
+#define IDC_DELETE_BUTTON               1018
+#define IDC_RESET_BUTTON                1019
+#define IDC_PLAYLIST                    1022
+#define IDC_NEXT_BUTTON                 1025
+#define IDC_AUTOPLAY_CHECK              1026
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32782
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
